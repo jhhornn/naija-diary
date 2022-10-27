9cc5679bbf11980eb7a7ca064e97bd72063d44bc
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-27 10:38:57 +0100

    db updated

--- config/database.js
+++ config/database.js
@@ -2,20 +2,20 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config({ path: "./config/.env" })
 
-const MONGODB_URI = process.env.MONGODB_URI //|| 'mongodb://0.0.0.0:27017/naija-diary'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/naija-diary'
 
-function connectDB() {
-  try {
-    mongoose.connect(MONGODB_URI)
-
-    mongoose.connection.on("connected", () => {
-      console.log("Connected to MongoDB Successfully")
+const connectDB = async () =>{
+  try{
+    const conn = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
     })
-  } catch {
-    mongoose.connection.on("error", (err) => {
-      console.log("An error occurred while connecting to MongoDB")
-      console.log(err)
-    })
+    console.log(`MongoDB Conected: ${conn.connection.host}`)
+  } catch (err) {
+    console.err(err)
+    process.exit(1)
   }
 }
 
