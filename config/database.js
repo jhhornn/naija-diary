0b0dc6fbeb54c42701cac830a9f21f19ba1714b7
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-29 09:19:32 +0100

    fix: database modifed for test cases

--- config/database.js
+++ config/database.js
@@ -2,21 +2,52 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config({ path: "./config/.env" })
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/naija-diary'
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
-const connectDB = async () =>{
-  try{
+
+const connectDB = async (next) => {
+  try {
     const conn = await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
+      useUnifiedTopology: true
     })
     console.log(`MongoDB Conected: ${conn.connection.host}`)
   } catch (err) {
+    next(err)
     console.err(err)
     process.exit(1)
   }
 }
 
-module.exports = connectDB
+
+//! hook functions for test database
+const dropDbBeforeEachTest = async (next) => {
+  try {
+    await mongoose.connect(MONGODB_URI)
+    await mongoose.connection.db.dropDatabase()
+  } catch (err) {
+    next(err)
+    console.error("Couldn't connect to MongoDB")
+  }
+}
+
+
+const dropDbAfterAllTestAndCloseConnection = async (next) => {
+  try {
+    await mongoose.connection.db.dropDatabase()
+    await mongoose.connection.close()
+  } catch (err) {
+    next(err)
+    console.error("Failed to drop database")
+  }
+}
+
+module.exports = {
+  dropDbBeforeEachTest,
+  dropDbAfterAllTestAndCloseConnection,
+  connectDB
+}
+
