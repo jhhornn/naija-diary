e2e491cb9f12872bc277b55f313a29772ce977ea
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-29 09:21:09 +0100

    new: dummy data to test user endpoint

---
+++ utils/testHelper.js
@@ -0,0 +1,46 @@
+const User = require('../models/users')
+
+
+//! Instantiating users test parameters
+const initialUsers = [
+    {
+        email: 'seun@gmail.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'Password',
+    },
+    {
+        email: 'tobi@gmail.com',
+        firstName: 'Tobi',
+        lastName: 'Doe',
+        password: 'Password2',
+    }
+]
+
+
+const nonExistingId = async () => {
+    const user = new User({ email: 'tempUser@gmail.com', firstName: 'Tobi', lastName: 'Doe', password: 'temppword' })
+    await user.save()
+    await user.remove()
+
+    return user._id.toString()
+}
+
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(note => note.toJSON())
+}
+
+const newUser = {
+    email: 'jhhornn@gmail.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    password: 'Password',
+}
+
+module.exports = {
+    initialUsers, 
+    nonExistingId, 
+    usersInDb,
+    newUser
+  }
