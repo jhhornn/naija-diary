724d557e6b632f87e789c995d38b76e1c59b4f38
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-30 14:27:14 +0100

    new: added error handler

---
+++ middlewares/errHandler.js
@@ -0,0 +1,35 @@
+const errorLogger = (err, req, res, next) => {
+  console.error("\x1b[31m", err)
+  next(err)
+}
+
+const errorResponder = (err, req, res, next) => {
+  res.header("Content-Type", "application/json")
+  if (err.name === "TypeError") {
+    res.status(401).json({
+      message: "unauthorised access"
+    })
+  } else if (err.name === "Error") {
+    res.status(401).json({
+      message: "unauthorised access"
+    })
+  } else if (err.message.startsWith("E11000 duplicate key error collection")) {
+    res.status(401).json({
+      message: "email already in use"
+    })
+  } else {
+    next(err)
+  }
+}
+
+const invalidPathHandler = (err, req, res, next) => {
+  res.status(500).send({
+    message: `${req.originalUrl} is not a valid path`
+  })
+}
+
+module.exports = {
+  errorLogger,
+  errorResponder,
+  invalidPathHandler
+}
