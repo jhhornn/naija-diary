5dd66cc429b06b52c0f1883b867741f4b45fb665
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-30 14:20:44 +0100

    update: added new test parameters for signup test

--- utils/testHelper.js
+++ utils/testHelper.js
@@ -1,46 +1,49 @@
-const User = require('../models/users')
-
+const User = require("../models/users")
 
 //! Instantiating users test parameters
 const initialUsers = [
-    {
-        email: 'seun@gmail.com',
-        firstName: 'John',
-        lastName: 'Doe',
-        password: 'Password',
-    },
-    {
-        email: 'tobi@gmail.com',
-        firstName: 'Tobi',
-        lastName: 'Doe',
-        password: 'Password2',
-    }
+  {
+    email: "seun@gmail.com",
+    firstName: "John",
+    lastName: "Doe",
+    password: "Password"
+  },
+  {
+    email: "tobi@gmail.com",
+    firstName: "Tobi",
+    lastName: "Doe",
+    password: "Password2"
+  }
 ]
 
-
 const nonExistingId = async () => {
-    const user = new User({ email: 'tempUser@gmail.com', firstName: 'Tobi', lastName: 'Doe', password: 'temppword' })
-    await user.save()
-    await user.remove()
+  const user = new User({
+    email: "tempUser@gmail.com",
+    firstName: "Tobi",
+    lastName: "Doe",
+    password: "temppword"
+  })
+  await user.save()
+  await user.remove()
 
-    return user._id.toString()
+  return user._id.toString()
 }
 
 const usersInDb = async () => {
-    const users = await User.find({})
-    return users.map(note => note.toJSON())
+  const users = await User.find({})
+  return users.map((note) => note.toJSON())
 }
 
 const newUser = {
-    email: 'jhhornn@gmail.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    password: 'Password',
+  email: "jhhornn@gmail.com",
+  firstName: "John",
+  lastName: "Doe",
+  password: "Password"
 }
 
 module.exports = {
-    initialUsers, 
-    nonExistingId, 
-    usersInDb,
-    newUser
-  }
+  initialUsers,
+  nonExistingId,
+  usersInDb,
+  newUser
+}
