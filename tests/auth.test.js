a0cb0f08a217314bdb5ed0391c45209edcb37489
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-31 19:45:42 +0100

    new: added test for login route

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -5,25 +5,32 @@ const app = require("../app")
 const api = supertest(app)
 const connectDB = require("../config/database")
 
-jest.setTimeout(30000)
+jest.setTimeout(50000)
 
-const User = require("../models/users")
+const UserModel = require("../models/users")
 
 beforeAll(async () => {
   await connectDB()
 })
 
 beforeEach(async () => {
-  await User.deleteMany({})
+  await UserModel.deleteMany({})
 
-  let userObject = new User(helper.initialUsers[0])
+  let userObject = new UserModel(helper.initialUsers[0])
   await userObject.save()
 
-  userObject = new User(helper.initialUsers[1])
+  userObject = new UserModel(helper.initialUsers[1])
   await userObject.save()
 })
 
 describe("Sign Up", () => {
+  test("sign up field must not be empty", async () => {
+    await api.post("/api/signup").send().expect(400)
+
+    const usersAtTheEnd = await helper.usersInDb()
+    expect(usersAtTheEnd).toHaveLength(helper.initialUsers.length)
+  })
+
   test("created a user and added to database successfully", async () => {
     await api
       .post("/api/signup")
@@ -33,6 +40,9 @@ describe("Sign Up", () => {
 
     const usersAtTheEnd = await helper.usersInDb()
     expect(usersAtTheEnd).toHaveLength(helper.initialUsers.length + 1)
+
+    const contents = usersAtTheEnd.map((n) => n.firstName)
+    expect(contents).toContain("John")
   })
 
   test("email must be unique", async () => {
@@ -42,16 +52,70 @@ describe("Sign Up", () => {
     expect(usersAtTheEnd).toHaveLength(helper.initialUsers.length)
   })
 
-  test("all required field must be met", async () => {
-    const { firstName } = helper.newUser
-    await api.post("/api/signup").send({ firstName }).expect(400)
+  test("all required field must be provided", async () => {
+    const { email, firstName } = helper.newUser
+    await api.post("/api/signup").send({ email, firstName }).expect(400)
 
     const usersAtTheEnd = await helper.usersInDb()
     expect(usersAtTheEnd).toHaveLength(helper.initialUsers.length)
   })
 })
 
+describe("Sign In", () => {
+  test("user exists in the database", async () => {
+    const { email, password } = helper.initialUsers[0]
+    await api
+      .post("/api/login")
+      .send({ email, password })
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+
+    const user = await UserModel.findOne({ email: email })
+    expect(user.comparePassword(password)).toBeTruthy()
+  })
+
+  test("user doesnt't exist in the database", async () => {
+    const { email, password } = helper.newUser
+    await api
+      .post("/api/login")
+      .send({ email, password })
+      .expect(401)
+      .expect("Content-Type", /application\/json/)
+
+    const user = await UserModel.findOne({ email: email })
+    expect(user).toBeNull()
+  })
+
+  test("password must be correct", async () => {
+    const { email } = helper.initialUsers[0]
+    await api
+      .post("/api/login")
+      .send({ email, password: "incorrect" })
+      .expect(401)
+      .expect("Content-Type", /application\/json/)
+
+    const user = await UserModel.findOne({ email })
+    await expect(user.comparePassword("incorrect")).resolves.toEqual(false)
+  })
+
+  test("email must be correct", async () => {
+    const { password } = helper.initialUsers[0]
+    await api
+      .post("/api/login")
+      .send({ email: "incorrect@gmail.com", password })
+      .expect(401)
+      .expect("Content-Type", /application\/json/)
+
+    const user = await UserModel.findOne({ email: "incorrect@gmail.com" })
+    expect(user).toBeNull()
+  })
+
+  test("both fields must be supplied", async () => {
+    await api.post("/api/login").send().expect(401)
+  })
+})
+
 afterAll(async () => {
-  await User.deleteMany({})
+  await UserModel.deleteMany({})
   await mongoose.connection.close()
 })
