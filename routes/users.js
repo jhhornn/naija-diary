a1c6dc4e4a9a6ed5df6eb988dd74f6a5fb2f954c
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-31 20:06:48 +0100

    new: added login users routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require("express")
-const userRouter = express.Router()
 const passport = require("passport")
-const signUp = require("../controllers/users")
+const { signUp, logIn } = require("../controllers/users")
+
+const userRouter = express.Router()
 
 userRouter
   .route("/signup")
   .post(passport.authenticate("signup", { session: false }), signUp)
+
+userRouter.route("/login").post(logIn)
 
 module.exports = userRouter
