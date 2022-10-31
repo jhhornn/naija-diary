d374793e8acd49b85e543660b9e6cd0a73a53d05
Awosise Oluwaseun <awosiseo@gmail.com>
2022-10-31 20:05:23 +0100

    new: added users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,8 @@
 const passport = require("passport")
 const jwt = require("jsonwebtoken")
+const path = require("path")
+const dotenv = require("dotenv")
+dotenv.config({ path: path.join(__dirname, "../config/.env") })
 
 const signUp = async (req, res, next) => {
   try {
@@ -12,4 +15,33 @@ const signUp = async (req, res, next) => {
   }
 }
 
-module.exports = signUp
+const logIn = async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err) {
+        return next(err)
+      }
+
+      if (!user) {
+        const error = new Error("Username or password is incorrect")
+        return next(error)
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error)
+
+        const body = { id: user.id, email: user.email }
+
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET, {
+          expiresIn: "1hr"
+        })
+
+        return res.status(200).json({ token })
+      })
+    } catch (err) {
+      next(err)
+    }
+  })(req, res, next)
+}
+
+module.exports = { signUp, logIn }
