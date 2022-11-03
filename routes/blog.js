a2a56d8acf8c5add0e472685505c0039284e79a1
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-03 07:23:47 +0100

    new: added routes to get and update blogs

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const createBlog = require("../controllers/blog")
+const { createBlog, getAllBlogs, updateBlog } = require("../controllers/blog")
 
 const blogRouter = express.Router()
 
 blogRouter.route("/blog").post(createBlog)
+blogRouter.route("/blog").get(getAllBlogs)
+blogRouter.route("/blog").put(updateBlog)
+
 
 module.exports = blogRouter
