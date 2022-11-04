da74ad0806017dcf47bcaa1f5ed6d2f9772b48c3
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-04 18:10:00 +0100

    update: errhandler to catch new error

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -5,7 +5,11 @@ const errorLogger = (err, req, res, next) => {
 
 const errorResponder = (err, req, res, next) => {
   res.header("Content-Type", "application/json")
-  if (err.name === "TypeError") {
+  if(err.message.startsWith("Cannot read properties of undefined (reading 'split')")){
+    res.status(400).json({
+      message: "tag query not found"
+    })
+  } else if (err.name === "TypeError") {
     res.status(401).json({
       message: "unauthorised access"
     })
