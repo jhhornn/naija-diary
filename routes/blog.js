de8117cb626ef4484a49fa44cdac933b51e9f33d
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-04 18:10:33 +0100

    update: protected create route

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,13 @@
 const express = require("express")
-const { createBlog, getAllBlogs, updateBlog } = require("../controllers/blog")
+const passport = require("passport")
+const { createBlog, getAllBlogs, updateBlog, updateBlogState } = require("../controllers/blog")
 
 const blogRouter = express.Router()
 
-blogRouter.route("/blog").post(createBlog)
+blogRouter.route("/blog").post(passport.authenticate('jwt', { session: false }), createBlog)
 blogRouter.route("/blog").get(getAllBlogs)
 blogRouter.route("/blog").put(updateBlog)
+blogRouter.route("/blog").patch(updateBlogState)
 
 
 module.exports = blogRouter
