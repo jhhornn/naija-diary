0ce79d47ec126f6ee83745c23909dd573f690f93
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-06 10:07:54 +0100

    fix: model should return _id

--- models/blog.js
+++ models/blog.js
@@ -61,7 +61,7 @@ BlogSchema.pre("save", function (next) {
 BlogSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     // returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
+    // delete returnedObject._id
     delete returnedObject.__v
   }
 })
