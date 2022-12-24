6fd174ae792941504968c3da64e685d379f0c364
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-24 01:06:25 +0100

    docs: swagger ui API documentation

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const passport = require("passport")
 const morgan = require("morgan")
 const { urlencoded } = require("body-parser")
-const { connectDB } = require("./config/database")
 const {
   errorLogger,
   errorResponder,
@@ -11,6 +10,10 @@ const {
 
 const usersRoute = require("./routes/users")
 const blogRoute = require("./routes/blog")
+
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./naija-diary.yaml")
 
 const app = express()
 
@@ -27,10 +30,11 @@ app.use("/api", blogRoute)
 app.get("/", (req, res, next) => {
   res.send(
     "<h1 style='color: black;text-align: center'>Welcome to <span style='color: green'>Naija Diary</span>!</h1>\
-     <br> <h3 style='color: black;text-align: center'>Click <a href='https://github.com/jhhornn/naija-diary'>here</a> to get started</h3>"
+     <br> <h3 style='color: black;text-align: center'>Click <a href='/api-docs'>here</a> to get started</h3>"
   )
   next()
 })
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.all("*", (req, res, next) => {
   next()
