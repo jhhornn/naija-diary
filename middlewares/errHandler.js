fe7b730b19ce7b7e6fee9faf3950abe9ac6f357d
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-27 15:43:04 +0100

    chore: moved handling of invalid path to app.js

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -4,36 +4,32 @@ const errorLogger = (err, req, res, next) => {
 }
 
 const errorResponder = (err, req, res, next) => {
- let customError = {
-  statusCode: err.statusCode || 500,
-  msg: err.message || "Something went wrong"
- }
+  let customError = {
+    statusCode: err.statusCode || 500,
+    msg: err.message || "Something went wrong"
+  }
 
- if (err.name == " ValidationError") {
-  customError.msg = Objects.values(err.errors).map((item) => item.message).join(",")
-  customError.statusCode = 400
- }
- if(err.code && err.code === 11000) {
-  customError.msg = `Duplicate value for ${Object.keys(err.keyValue)} field, please choose another value`
-  customError.statusCode = 400
- }
- if(err.name == "CastError") {
-  customError.msg = `No item with id: ${err.value}`
-  customError.statusCode = 404
- }
+  if (err.name == " ValidationError") {
+    customError.msg = Objects.values(err.errors)
+      .map((item) => item.message)
+      .join(",")
+    customError.statusCode = 400
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`
+    customError.statusCode = 400
+  }
+  if (err.name == "CastError") {
+    customError.msg = `No item with id: ${err.value}`
+    customError.statusCode = 404
+  }
 
- return res.status(customError.statusCode).json({ msg: customError.msg })
-
-}
-
-const invalidPathHandler = (err, req, res, next) => {
-  res.status(500).send({
-    message: `${req.originalUrl} is not a valid path`
-  })
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = {
   errorLogger,
-  errorResponder,
-  invalidPathHandler
+  errorResponder
 }
